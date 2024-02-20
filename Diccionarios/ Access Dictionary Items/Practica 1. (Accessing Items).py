080154bf286bdded60591fcaba26be75530872f0
Miguel-1152 <159779365+Miguel-1152@users.noreply.github.com>
2024-02-20 09:34:50 -0500

    Add files via upload

---
+++ Diccionarios/ Access Dictionary Items/Practica 1. (Accessing Items).py
@@ -0,0 +1,15 @@
+##Accessing items
+
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+x = thisdict.get("model")
+print(x)
+
+
+
+
+
+
---
+++ Diccionarios/ Access Dictionary Items/Practica 2. (Accessing Items).py
@@ -0,0 +1,20 @@
+##Accessing items
+
+car = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+
+
+
+
+
+
